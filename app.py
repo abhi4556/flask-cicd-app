d1f4568b37e1ddc9f02b9c72f8f47e683a5a58df
abhi4556 <abhijit.myworld@gmail.com>
2026-02-27 09:41:43 +0000

    Updated app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "CI/CD Working Successfully!"
+    return "New CI/CD Working Successfully!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
